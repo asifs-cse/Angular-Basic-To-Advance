ac6eda25de272df87f7a1bf64c665c7863d2404f
unknown <asifs.cse@gmail.com>
2023-04-16 15:43:17 +0530

    template css properties

--- Components/AngularComponents/src/app/notification/notification.component.ts
+++ Components/AngularComponents/src/app/notification/notification.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   template: '<div><p>This website uses cookies to provide better user experience</p></div>',
-  styleUrls: ['./notification.component.css']
+  styles: ["div{margin: 10px 0px; padding: 10px 20px; bakcground-color: #FAD7A0;text-align:center}","p{color: red}"]
 })
 export class NotificationComponent {
 
