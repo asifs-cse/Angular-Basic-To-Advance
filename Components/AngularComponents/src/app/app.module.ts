04f0077bee534c0e36d19a2e036bc8e5975c5c99
unknown <asifs.cse@gmail.com>
2023-04-16 15:27:37 +0530

    creaate component and run app

--- Components/AngularComponents/src/app/app.module.ts
+++ Components/AngularComponents/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ContainerComponent} from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
