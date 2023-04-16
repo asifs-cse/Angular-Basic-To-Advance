2ab87e6ff229647aa38d21f81f29bb3babbc91b0
unknown <asifs.cse@gmail.com>
2023-04-16 15:54:33 +0530

    install bootstrap

--- Components/AngularComponents/src/app/notification/notification.component.ts
+++ Components/AngularComponents/src/app/notification/notification.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: '<div><p>This website uses cookies to provide better user experience</p></div>',
+  template: '<div class="alert alert-success"><p>This website uses cookies to provide better user experience</p></div>',
   styles: ["div{margin: 10px 0px; padding: 10px 20px; bakcground-color: #FAD7A0;text-align:center}","p{color: red}"]
 })
 export class NotificationComponent {
