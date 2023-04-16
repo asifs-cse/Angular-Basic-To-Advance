d59cba4e84d05f48cbe4ad7f56f3e1c51f207db0
unknown <asifs.cse@gmail.com>
2023-04-16 15:36:11 +0530

    template propertise

--- Components/AngularComponents/src/app/app.module.ts
+++ Components/AngularComponents/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import {ContainerComponent} from './Container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
---
+++ Components/AngularComponents/src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<div><p>This website uses cookies to provide better user experience</p></div>',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+
+}
